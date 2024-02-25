862f33f40890f8d98e9485c08c7f7c6f57b85a88
drakenevadie19 <drakennievadje19@gmail.com>
2024-02-25 02:28:50 -0600

    New Change: Add GraphQL to server, but still figure out how to use

--- movie-maker-channel-be/server.js
+++ movie-maker-channel-be/server.js
@@ -3,6 +3,8 @@ const app = express();
 
 const mongoose = require("mongoose");
 
+const { graphqllHTTP } = require('graphql-http');
+
 app.get('/hi', (req, res) => {
     res.send('Hello from express server');
 });
@@ -15,9 +17,25 @@ mongoose.connect("mongodb+srv://thanhnguyen14gers:csz0dMPbQB1HcEF9@movie-maker-a
     })
     .catch((err) => {
         console.log('Error:', err);
-    })
- 
+    });
 
+var { graphql, buildSchema } = require("graphql")
+
+var schema = buildSchema(`
+    type Query {
+    hello: String
+    }
+`)
+
+var rootValue = { hello: () => "Hello world!" }
+
+var source = "{ hello }"
+
+graphql({ schema, source, rootValue }).then(response => {
+    console.log(response)
+})
+
+// app.get('/graphql')
 
 // When server run on port, it can listen
 app.listen('8000', () => {
