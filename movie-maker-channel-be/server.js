5a8745b173a06ec70d16c7f0fd7c20b359c33c41
drakenevadie19 <drakennievadje19@gmail.com>
2024-02-29 21:55:37 -0600

    New Changes: add graphql endpoints

--- movie-maker-channel-be/server.js
+++ movie-maker-channel-be/server.js
@@ -3,7 +3,9 @@ const app = express();
 
 const mongoose = require("mongoose");
 
-const { graphqllHTTP } = require('graphql-http');
+const { graphqlHTTP } = require('express-graphql');
+
+var { buildSchema } = require("graphql")
 
 app.get('/hi', (req, res) => {
     res.send('Hello from express server');
@@ -19,23 +21,57 @@ mongoose.connect("mongodb+srv://thanhnguyen14gers:csz0dMPbQB1HcEF9@movie-maker-a
         console.log('Error:', err);
     });
 
-var { graphql, buildSchema } = require("graphql")
+const movies = [
+    {
+        name: 'Home Alone 1',
+        genre: 'Funny',
+        year: '1990'
+    },
+    {
+        name: 'Home Alone 2',
+        genre: 'Funny',
+        year: '1992'
+    },
+    {
+        name: 'Home Alone 3',
+        genre: 'Funny',
+        year: '1997'
+    },
+    {
+        name: 'Home Alone 4',
+        genre: 'Funny',
+        year: '2002'
+    }
+]
 
 var schema = buildSchema(`
     type Query {
-        hello: String
+        listmovie: [Movie]
+    }
+
+    type Movie {
+        name: String, 
+        genre: String, 
+        year: String
     }
 `)
 
-var rootValue = { hello: () => "Hello world!" }
+var rootValue = { 
+    listmovie: () => {
+        return movies;
+    }, 
+    hello: () => "Hello World"
+};
 
-var source = "{ hello }"
+// graphql({ schema, source, rootValue }).then(response => {
+//     console.log(response)
+// })
 
-graphql({ schema, source, rootValue }).then(response => {
-    console.log(response)
-})
-
-// app.get('/graphql')
+app.use('/hello', graphqlHTTP({
+    schema, 
+    rootValue,
+    graphiql: true
+}))
 
 // When server run on port, it can listen
 app.listen('8000', () => {
