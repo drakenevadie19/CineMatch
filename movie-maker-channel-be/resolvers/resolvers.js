dce2cbf7a1cb9fbcc835c2e33cf212e149370973
drakenevadie19 <drakennievadje19@gmail.com>
2024-09-14 06:32:20 -0500

    New change: Create schema and connect to mongoDB (successfully tested)

--- movie-maker-channel-be/resolvers/resolvers.js
+++ movie-maker-channel-be/resolvers/resolvers.js
@@ -1,41 +1,46 @@
-
-const movies = [
-    {
-        name: 'Home Alone 1',
-        genre: 'Funny',
-        year: '1990'
-    },
-    {
-        name: 'Home Alone 2',
-        genre: 'Funny',
-        year: '1992'
-    },
-    {
-        name: 'Home Alone 3',
-        genre: 'Funny',
-        year: '1997'
-    },
-    {
-        name: 'Home Alone 4',
-        genre: 'Funny',
-        year: '2002'
-    }
-]
+// const movies = [
+//     {
+//         name: 'Home Alone 1',
+//         genre: 'Funny',
+//         year: '1990'
+//     },
+//     {
+//         name: 'Home Alone 2',
+//         genre: 'Funny',
+//         year: '1992'
+//     },
+//     {
+//         name: 'Home Alone 3',
+//         genre: 'Funny',
+//         year: '1997'
+//     },
+//     {
+//         name: 'Home Alone 4',
+//         genre: 'Funny',
+//         year: '2002'
+//     }
+// ]
 
 // Answer Schema
+
+const mongoModel = require('../models/model');
+
+
 const resolvers = { 
     // When being reached out, it will return a listMovies array/list, which contains of many Movie objects, each contain 3 fields: name, genre and year 
     listMovies: () => {
-        return movies;
+        return mongoModel.find({}); // Find everything inside this model
     }, 
     hello: () => "Hello World",
     addMovie: (args) => {
-        return {
-            // When ask for addMovie, return the name of added movie.
+        // Create data from provided/inputted data
+        let newMovieData = new mongoModel({
             name: args.name, 
             genre: args.genre, 
             year: args.year
-        } 
+        })
+        newMovieData.save();
+        return newMovieData;
     }
 };
 
