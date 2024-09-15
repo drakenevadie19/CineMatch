92bd4ac6422f5283c74cf0ed34cbed7437fad9e4
drakenevadie19 <drakennievadje19@gmail.com>
2024-09-14 23:27:19 -0500

    New Change: Adding mutation query

--- movie-maker-channel/src/components/add-movie.js
+++ movie-maker-channel/src/components/add-movie.js
@@ -1,15 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { HashLink as Link } from 'react-router-hash-link';
+import { gql, useMutation } from '@apollo/client';
+
+const listAllMovies = gql`
+    query {
+        listMovies {
+            name
+            genre
+            year
+        }
+    }
+`
+
+// Define a mutation named AddNewMovie
+// When click addMovie Button to fire mutation AddNewMovie
+const addNewMovie = gql`
+    mutation AddNewMovie($name: String!, $genre: String!, $year: String!) { 
+        addMovie(name: $name, genre: $genre, year: $year) {
+            name
+            genre
+            year
+        }
+    }
+`
 
 //Form to add new movie 
 const AddMovie = () => {
+    // Getting from form
+    const [movieName, setMovieName] = useState("");
+    const [movieGenre, setMovieGenre] = useState("");
+    const [movieYear, setMovieYear] = useState("");
+
+    // newMovieData is what we inputted to the form
+    const [newMovieData, { loading, error }] = useMutation(addNewMovie);
+
+    if (loading) return <p className='loading'>'Submitting...'</p>;
+    if (error) return <p className='error'>`Can not add your movie! ${error.message}`</p>;
+    
+    const submitMovie = (e) => {
+        e.preventDefault(); // Prevent default action of parent component
+        newMovieData({
+            variables: {
+                name: movieName,
+                genre: movieGenre,
+                year: movieYear
+            },
+            refetchQueries: [{
+                query: listAllMovies
+            }]
+        }) // => These fields will be added to mutation 
+        console.log("Movie Added!");
+    }
+
     return ( 
         <div className='addMovie' id="addNewMovie">
-            <form>
-                <input type='text' placeholder='Movie Name' required/>
-                <input type='text' placeholder='Movie Name' required/>
-                <input type='text' placeholder='Movie Name' required/>
+            <form onSubmit={submitMovie}>
+                <input type='text' placeholder='Movie Name' onChange={(e) => setMovieName(e.target.value)} required/>
+                <input type='text' placeholder='Movie Genre' onChange={(e) => setMovieGenre(e.target.value)} required/>
+                <input type='text' placeholder='Movie Year' onChange={(e) => setMovieYear(e.target.value)} required/>
 
                 <button>Add Movie</button>
             </form>
